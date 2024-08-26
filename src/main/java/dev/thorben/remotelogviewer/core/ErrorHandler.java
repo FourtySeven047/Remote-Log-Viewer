6ff71a087cf411db0253200378fe9584b1f40eb8
Thorben B. <thorbenbunger@gmail.com>
2024-08-26 17:39:44 +0200

    feat: add ErrorHandler class

---
+++ src/main/java/dev/thorben/remotelogviewer/core/ErrorHandler.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.thorben.remotelogviewer.core;
+
+import dev.thorben.remotelogviewer.frames.ErrorFrame;
+
+/**
+ *
+ * @author Thorben Buenger
+ */
+public class ErrorHandler {
+    
+    public static void handle(Exception e, String message) {
+        ErrorFrame frame = new ErrorFrame();
+        frame.setErrorMessage(message);
+        appendLog(e);
+    }
+    
+    private static void appendLog(Exception e) {
+        
+    }
+}
